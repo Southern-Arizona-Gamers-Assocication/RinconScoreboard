e598b8ab849862f29406f40aece1360298c9b44d
RobertStickney <RobertStickney@users.noreply.github.com>
2025-09-02 13:31:02 -0700

    Update sbSounds.py

--- sbSounds.py
+++ sbSounds.py
@@ -157,8 +157,8 @@ class sbSoundsMpSpawning(sbSounds, SpawnProcess):
     def __init__(self) -> None:
         """"""
         print(f"Executing: {self.__class__.__qualname__}.__init__()")
-        super(sbSounds).__init__()
-        super(SpawnProcess).__init__("Sounds")
+        super().__init__()
+        super(sbSounds).__init__("Sounds")
         print(f"Done Executing: {self.__class__.__qualname__}.__init__()")
 # End of class sbSounds
 
